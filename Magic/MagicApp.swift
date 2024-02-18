66483bb1ed544f4198f5fc9cf2964fbb3759bd71
ehsan2793 <ehsan2793@gmail.com>
2024-02-17 16:37:14 -0800

    Initial Commit

---
+++ Magic/MagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicApp.swift
+//  Magic
+//
+//  Created by Ehsan Rahimi on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
